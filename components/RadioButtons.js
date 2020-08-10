7e50bfa0c0ac6ca7fdeb2fb7359c43abbe0d061b
Samir Shrestha <theonlysamir@gmail.com>
2020-08-11 00:56:48 +0545

    Difficulty Level Added

--- components/RadioButtons.js
+++ components/RadioButtons.js
@@ -1,22 +1,69 @@
-import React from 'react';
+import React, { useState } from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import { RadioButton } from "react-native-paper";
 
-import {TouchableWithoutFeedback, Text} from 'react-native';
+export function CombosLevel() {
+  const [checked, setChecked] = useState("Easy");
 
-export default function radioButton({elements= []}) {
-    function selected(id){
-        elements.forEach(element =>{
-            element.id===id?element.selected=false:true; 
-        })
-    }
-    
-    return (
-        elements.map(element=>{
-            <TouchableWithoutFeedback style={{flex:1,height:30, width:100, backgroundColor:"#fff"}} onPress= {element.handleOnPress && selected(element.id)}  >
-                <Text style={{color:"#FFF"}}> {element.name} </Text> 
-                <Text> {element.selected?'!!!':'XXX'}</Text>
-            </TouchableWithoutFeedback>
-        
-        })
-    )
+  const LEVELS = [
+    {
+      key: "1",
+      name: "Easy",
+    },
+    {
+      key: "2",
+      name: "Medium",
+    },
+    {
+      key: "3",
+      name: "Hard",
+    },
+    {
+      key: "4",
+      name: "Expert",
+    },
+  ];
+
+  return (
+    <View>
+      <Text style={styles.text}>Combos Level </Text>
+      <FlatList
+        data={LEVELS}
+        renderItem={({ item }) => (
+          <View key={item.key} style={styles.radioView}>
+            <RadioButton
+              value={item.name}
+              color={"#04C3FF"}
+              uncheckedColor={"#C4C4C4"}
+              status={checked === item.name ? "checked" : "unchecked"}
+              onPress={() => {
+                setChecked(item.name);
+              }}
+            />
+
+            <Text style={styles.radioText}>{item.name}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
 }
 
+const styles = StyleSheet.create({
+  radioView: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  radioText: {
+    color: "#E5E5E5",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  text: {
+    color: "#E5E5E5",
+    fontSize: 25,
+    fontWeight: "bold",
+    margin: 5
+  },
+});
--- pages/SettingsPage.js
+++ pages/SettingsPage.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { View} from 'react-native';
 
 import {WorkTimeSlider, RestTimeSlider} from '../components/sliders'
+import {CombosLevel} from '../components/RadioButtons'
 
 
 export default function SettingsPage() {
     return (
-        <View style= {{width:"100%"}}>
+        <View style= {{flex:1, width:"100%"}}>
             <WorkTimeSlider />
             <RestTimeSlider />
+            <CombosLevel />
         </View>
     )
 }
